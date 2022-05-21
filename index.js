3cc929051022b876ee3b30a748232d032776a8fa
Ranjan Soumya <83495544+ranjansmy@users.noreply.github.com>
2022-05-21 11:48:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// Index.js is the entry point
+const express = require('express')
+const cors = require('cors')
+const ratelimit = require('express-rate-limit')
+require('dotenv').config()
+const errorHandler = require('./middleware/error')
+
+const PORT =  process.env.PORT || 5000  // Look for env variable PORT else go for 5000
+
+const app = express()
+
+// Rate Limiting
+const limiter = ratelimit({
+    windowsMs: 10 * 60 * 1000, //10 minutes
+    max: 100,
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// Enable Cors
+app.use(cors())
+
+// Set static Folder
+app.use(express.static('public'))
+
+//Routes
+app.use('/api', require('./routes'))
+
+// Error handler middleware
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
